26f817a6f62dc71b31e574bd6dcf66512bb6d916
Mostafijur Rahman <66906274+mostafijur-github@users.noreply.github.com>
2022-04-30 12:47:21 +0600

    Added html and css file

---
+++ demo.js
@@ -0,0 +1,11 @@
+function openNav() {    
+    document.getElementById("sidenav").style.width = "100%";
+    document.getElementById("main-body").style.paddingTop= "268px";
+    
+}
+function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+    document.getElementById("main-body").style.paddingTop= "0";
+
+    
+}
